1a948093f9c21c5015ac0b44bcd87353f34dc88c
Your Name <you@example.com>
2016-03-31 18:29:59 -0600

    Add index page, main script

---
+++ HW4/cgi-bin/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import cgitb
+cgitb.enable()
+
+print("Content-Type: text/html")
+print() 
+
+print("<TITLE>CGI script output</TITLE>")
+print("<H1>This is my first CGI script</H1>")
+print("Hello, world!")
